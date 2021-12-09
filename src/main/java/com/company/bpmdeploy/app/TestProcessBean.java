6767048937cb096e153118366994c085727be88e
Irina Lovtsova <i.lovtsova@haulmont.com>
2021-12-09 15:19:27 +0400

    change to second process

---
+++ src/main/java/com/company/bpmdeploy/app/TestProcessBean.java
@@ -0,0 +1,12 @@
+package com.company.bpmdeploy.app;
+
+import org.springframework.stereotype.Component;
+
+@Component(value = "demo_TestProcessBean")
+public class TestProcessBean {
+
+    public void testProcessMethod() {
+        //todo: add logger
+    }
+
+}
