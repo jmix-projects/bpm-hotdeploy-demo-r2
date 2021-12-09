c9d1d86ad1a5e73291554950b9deb6e34b0c485f
Irina Lovtsova <i.lovtsova@haulmont.com>
2021-12-09 15:40:40 +0400

    change bean in second process

--- src/main/java/com/company/bpmdeploy/app/TestProcessBean.java
+++ src/main/java/com/company/bpmdeploy/app/TestProcessBean.java
@@ -1,12 +1,16 @@
 package com.company.bpmdeploy.app;
 
 import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Component(value = "demo_TestProcessBean")
 public class TestProcessBean {
 
+    private static final Logger log = LoggerFactory.getLogger(TestProcessBean.class);
+
     public void testProcessMethod() {
-        //todo: add logger
+        log.info("----this is a message from second process----");
     }
 
 }
