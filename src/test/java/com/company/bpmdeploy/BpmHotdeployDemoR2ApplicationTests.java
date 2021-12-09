46fc797f556e1cace0719015a633e668cdbc69c7
Irina Lovtsova <i.lovtsova@haulmont.com>
2021-12-09 11:59:08 +0400

    init commit

---
+++ src/test/java/com/company/bpmdeploy/BpmHotdeployDemoR2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.bpmdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpmHotdeployDemoR2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
